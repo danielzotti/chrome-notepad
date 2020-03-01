bc5fb2ee4ee75692f067c6e3c77f61109618f252
Daniel Zotti <daniel.zotti87@gmail.com>
2020-03-01 14:27:53 +0100

    Changed folder structure

--- extension/options.js
+++ extension/options.js
@@ -3,16 +3,12 @@ const options = document.querySelectorAll('select');
 chrome.storage.local.get(['text', 'background'], function (data) {
     console.log('notepad data', data);
     options.forEach(option => {
-        // console.log(option.value);
-        // console.log(option.name);
-        // console.log(data[option.name]);
         option.value = data[option.name];
     });
 });
 
 options.forEach(option => {
     option.addEventListener('change', function (event) {
-        // console.log(event);
         const item = {};
         item[event.target.name] = event.target.value;
         saveItem(item);
